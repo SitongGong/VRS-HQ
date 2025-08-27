4d57b4c423d9fe023b334442f129f25f689c93d9
SitongGong <123962491+SitongGong@users.noreply.github.com>
2025-08-27 11:39:39 +0800

    Comment out vision tower type checks

--- model/llava/model/multimodal_encoder/builder.py
+++ model/llava/model/multimodal_encoder/builder.py
@@ -7,11 +7,11 @@ def build_vision_tower(vision_tower_cfg, **kwargs):
         "mm_vision_tower",
         getattr(vision_tower_cfg, "vision_tower", None),
     )
-    if (
-        vision_tower.startswith("openai")
-        or vision_tower.startswith("laion")
-        or "clip" in vision_tower
-    ):
-        return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
+    # if (
+    #     vision_tower.startswith("openai")
+    #     or vision_tower.startswith("laion")
+    #     or "clip" in vision_tower
+    # ):
+    return CLIPVisionTower(vision_tower, args=vision_tower_cfg, **kwargs)
 
-    raise ValueError(f"Unknown vision tower: {vision_tower}")
+    # raise ValueError(f"Unknown vision tower: {vision_tower}")
